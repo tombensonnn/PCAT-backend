356c69e36057ff3fdba4fc12d3f442516a529078
Can UYUMAZ <58149556+tombensonnn@users.noreply.github.com>
2022-04-05 18:54:53 +0300

    task completed

--- index.js
+++ index.js
@@ -1,3 +1,58 @@
-const fn = function () {
-  console.log('function working');
-};
+const express = require('express');
+const mongoose = require('mongoose');
+const fileUpload = require('express-fileupload');
+const methodOverride = require('method-override');
+const ejs = require('ejs');
+const app = express();
+const photoController = require('./controllers/photoControllers');
+const pageController = require('./controllers/pageController');
+
+
+// connect DB
+mongoose.connect('mongodb://localhost/pcat-db', { // eğer veritabanı varsa bağlanır yoksa oluşturur.
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log('DB CONNECTED');
+}).catch((error) => {
+  console.log(error);
+})
+// TEMPLATE ENGINE
+app.set('view engine', 'ejs');
+
+// MIDDLEWARES
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true })); // url'deki datayı okumamızı sağlar
+app.use(express.json()); // url'deki datayı json formatına çevirir.
+app.use(fileUpload());
+app.use(
+  methodOverride('_method', {
+    methods: ['POST', 'GET'],
+  })
+);
+
+// ROUTES
+
+// Photo Routes
+app.get('/', photoController.getAllPhotos);
+app.get('/photos/:id', photoController.getPhoto);
+app.post('/photos', photoController.createPhoto);
+app.put('/photos/:id', photoController.updatePhoto);
+app.delete('/photos/:id', photoController.deletePhoto);
+
+
+
+// Page Routes
+app.get('/about', pageController.getAboutPage);
+app.get('/add', pageController.getAddPage);
+app.get('/photos/edit/:id', pageController.getEditPage);
+
+
+
+
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Sunucu çalıştırıldı. Port: ${port}`);
+});
